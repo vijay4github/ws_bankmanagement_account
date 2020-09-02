04bf20e3cdc5313a652d2896e459e71989a6b9f1
VJ <vijaykumar.annur@gmail.com>
2020-09-02 21:32:41 +0530

    initial commit

---
+++ src/main/java/com/bankmanagement/account/configuration/RepositoryConfiguration.java
@@ -0,0 +1,15 @@
+package com.bankmanagement.account.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class RepositoryConfiguration implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+      //  config.exposeIdsFor(Loan.class);
+    }
+}
+
---
+++ src/test/java/com/bankmanagement/account/WsBankmanagementAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankmanagement.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsBankmanagementAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
